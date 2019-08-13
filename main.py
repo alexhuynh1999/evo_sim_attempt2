f2c2967d7d4892b95ef33f36e4bb2d0e17977ef8
Alex <alexhuynh1999@gmail.com>
2019-08-12 23:37:43 -0500

    weird bug where blocks phase past each other. after etsting i guess it has to do with the blocks being updated at the same time / not at the same time consistently

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import random
 pygame.init()
 
 # Display Settings
-width = 1600
-height = 1000
+width = 800
+height = 600
 display = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Evolution Simulator Attempt 2')
 clock = pygame.time.Clock()
@@ -20,18 +20,66 @@ class Block(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
         self.image = pygame.Surface([25,25])
-        self.image.fill(b)
+        self.image.fill(
+            (random.randrange(0, 255),
+            random.randrange(0, 255),
+            random.randrange(0, 255))
+        )
         self.rect = self.image.get_rect()
-        self.x_vel = random.randrange(-5,5)
-        self.y_vel = random.randrange(-5,5)
+        self.x_vel = random.randrange(-10.0, 10.0)
+        self.y_vel = 0 #random.randrange(-5,5)
         self.id = Block.id
         Block.id += 1
 
-    def reverse(self):
-        self.x_vel *= -1
-        self.y_vel *= -1
+    def hascollided(self, box):
+        # Collisions in the x-direction
+        if (self.x_vel > 0 and box.x_vel < 0) or (self.x_vel < 0 and box.x_vel > 0):
+            self.x_vel *= -1
+            box.x_vel *= -1
+        # Wonky physics, but at least the momentum is conserved
+        xp = self.x_vel + box.x_vel
+        if self.x_vel > 0 and box.x_vel > 0:
+            if self.x_vel > box.x_vel:
+                box.x_vel = 0.75 * xp
+                self.x_vel = 0.25 * xp
+            else:
+                box.x_vel = 0.25 * xp
+                self.x_vel = 0.75 * xp
+        if self.x_vel < 0 and box.x_vel < 0:
+            if self.x_vel > box.x_vel:
+                box.x_vel = 0.25 * xp
+                self.x_vel = 0.75 * xp
+            else:
+                box.x_vel = 0.75 * xp
+                self.x_vel = 0.25 * xp
+
+        if self.y_vel > 0 and box.y_vel < 0 or self.y_vel < 0 and box.y_vel > 0:
+            self.y_vel *= -1
+            box.y_vel *= -1
+        yp = self.y_vel + box.y_vel
+        if self.y_vel > 0 and box.y_vel > 0:
+            if self.y_vel > box.y_vel:
+                box.y_vel = 0.75 * yp
+                self.y_vel = 0.25 * yp
+            else:
+                box.y_vel = 0.25 * yp
+                self.y_vel = 0.75 * yp
+        if self.y_vel < 0 and box.y_vel < 0:
+            if self.y_vel > box.y_vel:
+                box.y_vel = 0.25 * yp
+                self.y_vel = 0.75 * yp
+            else:
+                box.y_vel = 0.75 * yp
+                self.y_vel = 0.25 * yp
 
     def update(self):
+        for box in block_list:
+            if self.id == box.id:
+                continue
+
+            collided = pygame.sprite.collide_rect(self, box)
+            if collided:
+                Block.hascollided(self, box)
         self.rect.y += self.y_vel
         self.rect.x += self.x_vel
         if self.rect.x > width - 25 or self.rect.x < 0:
@@ -39,24 +87,15 @@ class Block(pygame.sprite.Sprite):
         if self.rect.y > height - 25 or self.rect.y < 0:
             self.y_vel *= -1
 
-        for box in block_list:
-            if self.id == box.id:
-                continue
-
-            collided = pygame.sprite.collide_rect(self, box)
-            if collided:
-                Block.reverse(self)
-                Block.reverse(box)
-
 
 all_sprites_list = pygame.sprite.Group()
 
 # Creating blocks
 block_list = pygame.sprite.Group()
-for x in range(10):
+for x in range(2):
     block = Block(b, 50, 50)
     block.rect.x = random.randrange(width - 25)
-    block.rect.y = random.randrange(height - 25)
+    block.rect.y = 400 #random.randrange(height - 25)
     block_list.add(block)
     all_sprites_list.add(block)
 
