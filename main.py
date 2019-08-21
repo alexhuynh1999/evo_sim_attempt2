9adccd072753cd2f636e97438e3091c35712a75b
Alex Huynh <alexhuynh1999@gmail.com>
2019-08-20 23:43:17 -0500

    they can mate!

--- main.py
+++ main.py
@@ -16,22 +16,20 @@ clock = pygame.time.Clock()
 b = (0, 0, 0)
 w = (255, 255, 255)
 
+population = 10
 
 class Block(pygame.sprite.Sprite):
     id = 0
 
-    def __init__(self, color, width, height):
+    def __init__(self, color, vision, x_vel, y_vel):
         super().__init__()
         self.image = pygame.Surface([25, 25])
-        self.image.fill(
-            (random.randrange(0, 255),
-             random.randrange(0, 255),
-             random.randrange(0, 255))
-        )
+        self.image.fill(color)
+        self.color = color
         self.rect = self.image.get_rect()
-        self.x_vel = random.randrange(1, 10)
-        self.y_vel = random.randrange(1, 10)
-        self.vision = random.randrange(50, 250)
+        self.vision = vision
+        self.x_vel = x_vel
+        self.y_vel = y_vel
         self.id = Block.id
         Block.id += 1
         self.fitness = 0
@@ -46,7 +44,7 @@ class Block(pygame.sprite.Sprite):
         if self.rect.y > height - 25 or self.rect.y < 0:
             self.y_vel *= -1
 
-        pygame.draw.circle(display,(255,0,0), (self.rect.x, self.rect.y), self.vision, 4)
+        pygame.draw.circle(display, (255, 0, 0), (self.rect.x, self.rect.y), self.vision, 4)
 
         closest_food = []
         for food in food_list:
@@ -75,6 +73,9 @@ class Block(pygame.sprite.Sprite):
         self.rect.x += projection[0]
         self.rect.y += projection[1]
 
+    def get_color(self):
+        return self.color
+
 
 class Food(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
@@ -99,8 +100,10 @@ def createFood(amount):
 
 
 # Creating blocks
-for x in range(10):
-    block = Block(b, 50, 50)
+for x in range(population):
+    block = Block((random.randrange(0, 255),
+                   random.randrange(0, 255),
+                   random.randrange(0, 255)), random.randrange(50, 250), random.randrange(1, 10), random.randrange(1, 10))
     block.rect.x = random.randrange(width - 25)
     block.rect.y = random.randrange(height - 25)
     block_list.add(block)
@@ -118,12 +121,41 @@ def thanos():
     for block in block_list:
         fitnessgram.append([block.id, block.fitness])
     fitnessgram.sort(key=sortSecond)
-    dead = fitnessgram[:len(fitnessgram)//2]
-    alive = fitnessgram[len(fitnessgram)//2:]
+    dead = fitnessgram[:len(fitnessgram) // 2]
     dead_id = [id[0] for id in dead]
     for block in block_list:
         if block.id in dead_id:
             block.kill()
+
+
+# Take a list of survivors, organized by fitness score
+# Have the top 4 mate with each other (#4 -> #3; #2 -> #1)
+# The parents will pass on these genes: color, vision, and speed. For admin purposes, they will also get a new ID
+def mate():
+    alive = []
+    for block in block_list:
+        alive.append([block, block.fitness, block.color, block.vision, (block.x_vel, block.y_vel)])
+    alive.sort(key=sortSecond)
+    for i in range(int(population/2) - 1):
+        color = [(alive[i][2][0] + alive[i + 1][2][0]) / 2,
+                 (alive[i][2][1] + alive[i + 1][2][1]) / 2,
+                 (alive[i][2][2] + alive[i + 1][2][2]) / 2]
+        vision = int((alive[i][3] + alive[i + 1][3]) / 2)
+        speed = [(alive[i][4][0] + alive[i + 1][4][0]) / 2,
+                 (alive[i][4][1] + alive[i + 1][4][1]) / 2]
+        block = Block(color, vision, speed[0], speed[1])
+        block.rect.x = random.randrange(width - 25)
+        block.rect.y = random.randrange(height - 25)
+        block_list.add(block)
+        all_sprites_list.add(block)
+
+
+def new_day():
+    thanos()
+    mate()
+    for block in block_list:
+        block.fitness = 0
+    createFood(25)
 
 
 # Running the sim
@@ -134,8 +166,7 @@ while on:
             on = False
 
     if len(food_list) == 0:
-        thanos()
-        createFood(25)
+        new_day()
         day += 1
         pygame.display.set_caption('Evolution Simulator | Day ' + str(day))
 
